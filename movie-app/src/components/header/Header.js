0e13f44323d8e2ee41ab6d9f7a79ac8949c9463c
swapna191919 <swapnajikkula@gmail.com>
2024-06-01 19:36:36 +0530

    formatted

--- movie-app/src/components/header/Header.js
+++ movie-app/src/components/header/Header.js
@@ -1,28 +1,3 @@
-// import React from "react"
-// import "./Header.css"
-// import { Link } from "react-router-dom"
-
-// const Header = () => {
-//     return (
-//         <div className="header">
-//               <img src="https://tse1.mm.bing.net/th?id=OIP.QDmT8QyI5_Lg4IlAIIp8lgHaGM&pid=Api&P=0&h=180" alt="logo" className="logo-img"  />
-//             <h1 className="h1">Movies</h1>
-//             <div className="headerLeft">
-          
-
-//                 <Link to="/"  style={{textDecoration: "none"}}><span>Home</span></Link>
-//                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-//                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-//                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Header
-
-
-
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
@@ -35,10 +10,10 @@ const Header = () => {
                 <h1 className="h1">Movies</h1>
             </div>
             <div className="headerLeft">
-                <Link to="/" style={{textDecoration: "none"}}><span>Home</span></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+                <Link to="/" style={{ textDecoration: "none" }}><span>Home</span></Link>
+                <Link to="/movies/popular" style={{ textDecoration: "none" }}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{ textDecoration: "none" }}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
             </div>
         </div>
     );
